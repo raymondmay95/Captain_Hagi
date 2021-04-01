55c4ef1115bd98b5d48e7895cf0e61dfe93cfa63
raymondmay95 <raymond.may.95@outlook.com>
2021-03-31 15:25:31 -1000

    added coords to the store

--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -1,72 +1,78 @@
-import React, { useState, useEffect } from "react";
-import getFetch from "../services/fetch";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { setCOORDSThunk } from "../store/coords";
+// import getFetch from "../services/fetch";
 // import * as geolib from "geolib";
 
 function Home() {
-  const [loaded, setLoaded] = useState("uninitiated");
-  const [myWeather, setMyWeather] = useState(null);
-  const [hourlyUrl, setHourlyUrl] = useState("");
-  const [forecastUrl, setForecastUrl] = useState("");
-  const [locationText, setLocationText] = useState(null);
-  const [weatherMeta, setWeatherMeta] = useState(null);
-  // const [latitude, setLatitude] = useState(null);
-  // const [longitude, setLongitude] = useState(null);
-
+  // eslint-disable-next-line
+  const [loaded, setLoaded] = useState(false);
+  const dispatch = useDispatch();
   useEffect(() => {
-    if (loaded === "finished") return;
-    setLoaded("fetching location");
-    if (!navigator.geolocation) return "no geolocation avalible";
-    async function fetchCoords() {
-      const sucessfullLookup = async (postion) => {
-        const { latitude, longitude } = await postion.coords;
-
-        setLoaded("fetching weather");
-        const fetchWeather = await fetch(
-          `https://api.weather.gov/points/${latitude},${longitude}`
-        );
-        if (fetchWeather.ok) {
-          const weather = await fetchWeather.json();
-          setMyWeather(weather);
-        } else {
-          console.log(`couldn't fetch weather at ${latitude}, ${longitude}`);
-        }
+    async function getCoords() {
+      const success = async (position) => {
+        const { latitude, longitude } = await position.coords;
+        let new_obj = { latitude, longitude };
+        return dispatch(setCOORDSThunk(new_obj)).then(() => setLoaded(true));
       };
-      navigator.geolocation.getCurrentPosition(sucessfullLookup, (e) =>
-        console.log(e)
-      );
-      if (myWeather) {
-        setLoaded("setting properties");
-        const { properties } = myWeather;
-        const { forecast, forecastHourly, relativeLocation } = properties;
-        // console.log(forecast, forecastHourly, relativeLocation.properties);
-        setLocationText(
-          relativeLocation.properties.city +
-            ", " +
-            relativeLocation.properties.state
-        );
-        setForecastUrl(forecast);
-        setHourlyUrl(forecastHourly);
-        setLoaded("finished");
-      }
-      async function getForcast(daily, hourly) {
-        let daily_meta = await getFetch(daily);
-        let hourly_meta = await getFetch(hourly);
-        let weather_meta = { daily_meta, hourly_meta };
-        setWeatherMeta(weather_meta);
-        return weather_meta;
-      }
-      let whole_forcast = await getForcast(forecastUrl, hourlyUrl);
-      console.log(whole_forcast);
+      navigator.geolocation.getCurrentPosition(success, (e) => console.log(e));
     }
-    fetchCoords();
-  });
+    return getCoords();
+  }, [dispatch]);
+
+  // useEffect(() => {
+  //   if (loaded === "finished") return;
+  //   setLoaded("fetching location");
+  //   if (!navigator.geolocation) return "no geolocation avalible";
+  //   async function fetchCoords() {
+  //     const sucessfullLookup = async (postion) => {
+  //       const { latitude, longitude } = await postion.coords;
+
+  //       setLoaded("fetching weather");
+  //       if (!latitude && !longitude) return;
+  //       const fetchWeather = await fetch(
+  //         `https://api.weather.gov/points/${latitude},${longitude}`
+  //       );
+  //       if (fetchWeather.ok) {
+  //         const weather = await fetchWeather.json();
+  //         setMyWeather(weather);
+  //       } else {
+  //         console.log(`couldn't fetch weather at ${latitude}, ${longitude}`);
+  //       }
+  //     };
+  //     navigator.geolocation.getCurrentPosition(sucessfullLookup, (e) =>
+  //       console.log(e)
+  //     );
+  //     if (myWeather) {
+  //       setLoaded("setting properties");
+  //       const { properties } = myWeather;
+  //       const { forecast, forecastHourly, relativeLocation } = properties;
+  //       // console.log(forecast, forecastHourly, relativeLocation.properties);
+  //       setLocationText(
+  //         relativeLocation.properties.city +
+  //           ", " +
+  //           relativeLocation.properties.state
+  //       );
+  //       setForecastUrl(forecast);
+  //       setHourlyUrl(forecastHourly);
+  //       setLoaded("finished");
+  //     }
+  //     async function getForcast(daily, hourly) {
+  //       let daily_meta = await getFetch(daily);
+  //       let hourly_meta = await getFetch(hourly);
+  //       let weather_meta = { daily_meta, hourly_meta };
+  //       setWeatherMeta(weather_meta);
+  //       return weather_meta;
+  //     }
+  //     let whole_forcast = await getForcast(forecastUrl, hourlyUrl);
+  //     console.log(whole_forcast);
+  //   }
+  //   fetchCoords();
+  // }, [loaded, myWeather, forecastUrl, hourlyUrl]);
   return (
     <div className="App">
       <header>
-        <p>{locationText ? locationText : null}</p>
-        <p>{loaded}</p>
-        <p>{myWeather ? "success" : "fail"}</p>
-        <p>{weatherMeta ? "Daily Forcast" : null}</p>
+        <p>{loaded ? "true" : "false"}</p>
       </header>
     </div>
   );
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,15 +4,19 @@ import { Provider } from "react-redux";
 import configureStore from "./store";
 import "./index.css";
 import App from "./App";
+import * as session from "./store/session";
+import * as coords from "./store/coords";
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== "production") {
   window.store = store;
+  window.session = session;
+  window.coords = coords;
 }
 
 const Root = () => (
-  <Provider store={store}>
+  <Provider store={store} session={session} coords={coords}>
     <App />
   </Provider>
 );
